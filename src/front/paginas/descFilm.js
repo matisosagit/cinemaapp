c893d066ab10846db3dad5206b1785fe3090a352
matisosagit <matiassosa1314@gmail.com>
2024-11-11 11:35:04 -0300

    primer commit

---
+++ src/front/paginas/descFilm.js
@@ -0,0 +1,17 @@
+import React from "react"; 
+import {Armchair} from "@phosphor-icons/react";
+
+export default function FilmDesc()  {
+    return(
+        <div>
+            <main>
+
+            </main>
+            <ahead>
+                <Armchair size={32} />
+                <p>aa</p>
+            </ahead>
+            
+        </div>
+    );
+}
---
+++ src/front/paginas/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Homepage = () => {
+    return(
+        <div>
+            <h1>Cine de prueba</h1>
+            <section>
+                <header>Cartelera</header>
+                
+            </section>
+        </div>
+    );
+}
